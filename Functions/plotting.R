1b9b9befc2dd1db6115f788b037a5002fde010fd
oscar cobb <you@example.comnewc6872@ox.ac.uk>
2024-12-10 17:19:18 +0000

    updates to code

--- Functions/plotting.R
+++ Functions/plotting.R
@@ -16,8 +16,8 @@ Plot_point<- function (Mass_flipper_data){
   Mass_flipper_data %>%
     ggplot(aes(x= body_mass_g,y = flipper_length_mm))+
     geom_point()+
-    geom_smooth(method = "lm", color = "black", linewidth = 0.8)+
-    labs (x= "Body Mass (mm)", y = "Flipper Length (mm)", title="Body mass vs flipper length in palmers Penguins", caption = "Grey Shaded Area indicates 95% CI")+
+    geom_smooth(method = "lm", color = "Red", linewidth = 0.8, fill = "pink")+
+    labs (x= "Body Mass (mm)", y = "Flipper Length (mm)", title="Body mass vs flipper length in palmers Penguins", caption = "Pink Shaded Area indicates 95% CI")+
     theme_bw()
 }
 
